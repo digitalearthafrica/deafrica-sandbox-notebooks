25bdb1051d92b7d7ead216c67f1c15c52f81ab12
eefaye <45861242+eefaye@users.noreply.github.com>
2021-02-15 13:24:22 +1100

    Delete DCAL directory (#182)
    Deleting DCAL directory as it is no longer used or useful.

--- DCAL/DCAL_utils/sort.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def xarray_sortby_coord(dataset, coord):
-    """
-    Sort an xarray.Dataset by a coordinate. xarray.Dataset.sortby() sometimes fails, so this is an alternative.
-    Credit to https://stackoverflow.com/a/42600594/5449970.
-    """
-    return dataset.loc[{coord:np.sort(dataset.coords[coord].values)}]
