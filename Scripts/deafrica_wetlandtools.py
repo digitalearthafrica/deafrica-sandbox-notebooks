315af40cd41215c420f38f1387fc4e6409b56fa4
chad <chad.burton@ga.gov.au>
2021-02-22 01:39:22 +0000

    drop 'tools' from the script names

--- Scripts/deafrica_wetlandtools.py
+++ Scripts/deafrica_wetlandtools.py
@@ -10,4 +10,4 @@ current_dir = pathlib.Path(__file__).parent.absolute()
 import sys
 sys.path.insert(1, str(current_dir.parent.absolute() / 'Tools'))
 
-from deafrica_tools.wetlandstools import *
+from deafrica_tools.wetland import *
