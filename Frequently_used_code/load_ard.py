b532a5c87d5b20376c2f51c9ef81e9f4b07016d5
cbur24 <c.a.burton123@gmail.com>
2021-03-26 02:24:18 +0000

    handling scaling and aliases better

--- Frequently_used_code/load_ard.py
+++ Frequently_used_code/load_ard.py
@@ -87,7 +87,7 @@ def load_ard(
     ls7_slc_off=True,
     predicate=None,
     dtype="auto",
-    scaling="raw",
+    verbose=True,
     **kwargs,
 ):
     """
@@ -140,10 +140,11 @@ def load_ard(
         calculation. Default is None, which will apply the following masks:
 
         for USGS Collection 2:
-
-        >>> {'cloud_shadow': 'not_cloud_shadow',
-        ...  'cloud_or_cirrus': 'not_cloud_or_cirrus',
-        ...  'nodata': False}
+        {
+         "clear": True,
+         "cloud_shadow": "not_high_confidence",
+         "nodata": False
+         }
 
     mask_pixel_quality : bool, optional
         An optional boolean indicating whether to apply the good data
@@ -175,13 +176,10 @@ def load_ard(
         be returned in the native data type of the data. Be aware that
         if data is loaded in its native dtype, nodata and masked
         pixels will be returned with the data's native nodata value
-        (typically -999), not NaN.
-    scaling : str, optional
-        If 'normalised', then surface reflectance values are scaled from
-        their original values to 0-1.  If 'raw' then dataset is returned
-        in its native scaling. WARNING: USGS Landsat Collection 2
-        surface reflectance values have an offset so normalised band indices
-        will return non-sensical results if setting scaling='raw'.
+        (typically -999), not NaN. NOTE: If loading Landsat, the data is
+        automatically rescaled so 'native' dtype will return a value error.
+    verbose : bool, optional
+        If True, print progress statements during data load
     **kwargs :
         A set of keyword arguments to `dc.load` that define the
         spatiotemporal query used to extract data. This typically
@@ -211,16 +209,17 @@ def load_ard(
     # We deal with `dask_chunks` separately
     dask_chunks = kwargs.pop("dask_chunks", None)
     requested_measurements = kwargs.pop("measurements", None)
-
+    
     # Warn user if they combine lazy load with min_gooddata
-    if (min_gooddata > 0.0) and dask_chunks is not None:
-        warnings.warn(
-            "Setting 'min_gooddata' percentage to > 0.0 "
-            "will cause dask arrays to compute when "
-            "loading pixel-quality data to calculate "
-            "'good pixel' percentage. This can "
-            "slow the return of your dataset."
-        )
+    if verbose:
+        if (min_gooddata > 0.0) and dask_chunks is not None:
+            warnings.warn(
+                "Setting 'min_gooddata' percentage to > 0.0 "
+                "will cause dask arrays to compute when "
+                "loading pixel-quality data to calculate "
+                "'good pixel' percentage. This can "
+                "slow the return of your dataset."
+            )
 
     # Verify that products were provided and determine if Sentinel-2
     # or Landsat data is being loaded
@@ -233,24 +232,64 @@ def load_ard(
         product_type = "ls"
     elif all(["s2" in product for product in products]):
         product_type = "s2"
-
+    
+    #Check some parameters before proceeding
+    if (product_type=="ls") & (dtype=='native'):
+        raise ValueError("Cannot load Landsat bands in native dtype "
+                         "as values require rescaling which converts to float")
+        
+    if (product_type=="ls") & (pq_categories_ls is not None):
+        if any(k in pq_categories_ls for k in ("cirrus","cirrus_confidence")):
+            raise ValueError("'cirrus' categories for the pixel quality mask"
+                             " are not supported by load_ard")
+    
     # If `measurements` are specified but do not include pixel quality bands,
     #  add these to `measurements` according to collection
-    if (product_type == "ls"):
-        print("Using pixel quality parameters for USGS Collection 2")
-        fmask_band = "quality_pixel"
+    if product_type == "ls":
+        if verbose:
+            print("Using pixel quality parameters for USGS Collection 2")
+        fmask_band = "pixel_quality"
 
     elif product_type == "s2":
-        print("Using pixel quality parameters for Sentinel 2")
+        if verbose:
+            print("Using pixel quality parameters for Sentinel 2")
         fmask_band = "SCL"
-
+    
     measurements = requested_measurements.copy() if requested_measurements else None
+    
+    #define a lits of acceptable aliases to load landsat. We can't rely on 'common'
+    #measurements as native band names have same name for different measurements.
+    ls_aliases = ['red','green','blue','nir', 'swir_1','swir_2','surface_temperature',
+                  'thermal_radiance','upwell_radiance','downwell_radiance',
+                  'atmospheric_transmittance','emissivity','emissivity_stddev',
+                  'pixel_quality','radiometric_saturation', 'cloud_distance',
+                  'surface_temperature_quality']
+    
+    if measurements is not None:
+        if product_type == "ls":
+            
+            #check we aren't loading aerosol bands from LS8
+            aerosol_bands = ['aerosol_qa','qa_aerosol','coastal_aerosol','SR_QA_AEROSOL']
+            if any(b in aerosol_bands for b in measurements):
+                raise ValueError("load_ard doesn't support loading aerosol related bands" 
+                                 " for Landsat-8, instead use dc.load()")
+            
+            #check measurements are in acceptable aliases list for landsat
+            if set(measurements).issubset(ls_aliases):
+                pass
+            else:
+                raise ValueError("load_ard does not support all band aliases for Landsat, "
+                            "use the following band names to load Landsat data: "
+                            + str(ls_aliases))
 
     # Deal with "load all" case: pick a set of bands common across
     # all products
     if measurements is None:
-        measurements = _common_bands(dc, products)
-
+        if product_type == "ls":
+            measurements = ls_aliases
+        else:
+            measurements = _common_bands(dc, products)
+    
     # If `measurements` are specified but do not include pq, add.
     if measurements:
         if fmask_band not in measurements:
@@ -272,16 +311,19 @@ def load_ard(
     dataset_list = []
 
     # Get list of datasets for each product
-    print("Finding datasets")
+    if verbose:
+        print("Finding datasets")
     for product in products:
 
         # Obtain list of datasets for product
-        print(f"    {product}")
+        if verbose:
+            print(f"    {product}")
         datasets = dc.find_datasets(product=product, **query)
 
         # Remove Landsat 7 SLC-off observations if ls7_slc_off=False
         if not ls7_slc_off and product in ["ls7_c2l2"]:
-            print("    Ignoring SLC-off observations for ls7")
+            if verbose:
+                print("    Ignoring SLC-off observations for ls7")
             datasets = [
                 i
                 for i in datasets
@@ -302,7 +344,8 @@ def load_ard(
     # If predicate is specified, use this function to filter the list
     # of datasets prior to load
     if predicate:
-        print(f"Filtering datasets using filter function")
+        if verbose:
+            print(f"Filtering datasets using filter function")
         dataset_list = [ds for ds in dataset_list if predicate(ds)]
 
     # Raise exception if filtering removes all datasets
@@ -362,7 +405,8 @@ def load_ard(
     if min_gooddata > 0.0:
 
         # Compute good data for each observation as % of total pixels
-        print("Counting good quality pixels for each time step")
+        if verbose:
+            print("Counting good quality pixels for each time step")
         data_perc = pq_mask.sum(axis=[1, 2], dtype="int32") / (
             pq_mask.shape[1] * pq_mask.shape[2]
         )
@@ -373,11 +417,12 @@ def load_ard(
         total_obs = len(ds.time)
         ds = ds.sel(time=keep)
         pq_mask = pq_mask.sel(time=keep)
-        print(
-            f"Filtering to {len(ds.time)} out of {total_obs} "
-            f"time steps with at least {min_gooddata:.1%} "
-            f"good quality pixels"
-        )
+        if verbose:
+            print(
+                f"Filtering to {len(ds.time)} out of {total_obs} "
+                f"time steps with at least {min_gooddata:.1%} "
+                f"good quality pixels"
+            )
 
     ###############
     # Apply masks #
@@ -386,7 +431,8 @@ def load_ard(
     # Generate good quality data mask
     mask = None
     if mask_pixel_quality:
-        print("Applying pixel quality/cloud mask")
+        if verbose:
+            print("Applying pixel quality/cloud mask")
         mask = pq_mask
 
     # Split into data/masks bands, as conversion to float and masking
@@ -421,54 +467,49 @@ def load_ard(
     if requested_measurements:
         ds = ds[requested_measurements]
 
-    # Scale data Sentinel-2 data 0-1 if requested
-    if scaling == "normalised":
-        if product_type == "s2":
-            print("Re-scaling Sentinel-2 data")
-            not_sr_bands = [
-                "scl",
-                "qa",
-                "mask",
-                "water_vapour",
-                "aerosol_optical_thickness",
-            ]
-            
-            for band in ds.data_vars:
-                if band not in not_sr_bands:
-                    ds[band] = ds[band] / 10000
-
     # Collection 2 Landsat raw values aren't useful so always rescale,
     # need different factors for surface-temp and SR
     if product_type == "ls":
-        print("Re-scaling Landsat C2 data")
-        not_sr_bands = [
-            "thermal_radiance",
-            "upwell_radiance",
-            "upwell_radiance",
-            "atmospheric_transmittance",
-            "emissivity",
-            "emissivity_stdev",
-            "cloud_distance",
-            "quality_aerosol",
-            "quality_surface_temperature",
-            "quality_pixel",
-            "surface_temperature",
-            "quality_radiometric_saturation",
-            "quality_surface_temperature"
-        ]
-
+        if verbose:
+            print("Re-scaling Landsat C2 data")
+        
+        sr_bands = ['red','green','blue','nir', 'swir_1','swir_2']
+        radiance_bands = ['thermal_radiance','upwell_radiance','downwell_radiance']
+        trans_emiss = ['atmospheric_transmittance','emissivity','emissivity_stddev']
+        qa = ['pixel_quality', 'radiometric_saturation']
+        
         for band in ds.data_vars:
-            if band == "surface_temperature":
-                ds[band] = ds[band] * 0.00341802 + 149.0
-
-            if band not in not_sr_bands:
+            if band == 'cloud_distance':
+                ds[band] =  0.01 * ds[band]
+                ds[band] = odc.algo.to_float(ds[band], dtype='float32')
+            
+            if band == 'surface_temperature_quality':
+                ds[band] =  0.01 * ds[band]
+                ds[band] = odc.algo.to_float(ds[band], dtype='float32')
+            
+            if band in radiance_bands:
+                ds[band] =  0.001 * ds[band]
+                ds[band] = odc.algo.to_float(ds[band], dtype='float32')
+            
+            if band in trans_emiss:
+                ds[band] =  0.0001 * ds[band]
+                ds[band] = odc.algo.to_float(ds[band], dtype='float32')
+            
+            if band in sr_bands:
                 ds[band] =  2.75e-5 * ds[band] - 0.2
-
+                ds[band] = odc.algo.to_float(ds[band], dtype='float32')
+            
+            if band == 'surface_temperature':
+                ds[band] = ds[band] * 0.00341802 + 149.0
+                ds[band] = odc.algo.to_float(ds[band], dtype='float32')
+                
     # If user supplied dask_chunks, return data as a dask array without
     # actually loading it in
     if dask_chunks is not None:
-        print(f"Returning {len(ds.time)} time steps as a dask array")
+        if verbose:
+            print(f"Returning {len(ds.time)} time steps as a dask array")
         return ds
     else:
-        print(f"Loading {len(ds.time)} time steps")
+        if verbose:
+            print(f"Loading {len(ds.time)} time steps")
         return ds.compute()
