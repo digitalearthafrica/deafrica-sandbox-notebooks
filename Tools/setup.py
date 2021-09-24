dc24d3696a102030d24aa7180e2ac67733dc37ae
cbur24 <c.a.burton123@gmail.com>
2021-09-24 00:55:39 +0000

    add "here" variable

--- Tools/setup.py
+++ Tools/setup.py
@@ -71,6 +71,8 @@ REQUIRES_PYTHON = '>=3.6.0'
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+here = os.path.abspath(os.path.dirname(__file__))
+
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
