13d3aa8dd2c17382dd0352f465fee32580ca0e21
eefaye <45861242+eefaye@users.noreply.github.com>
2021-10-08 11:23:09 +1100

    Added app subpackage. (#292)
    * Added app subpackage.
    
    * Added deprecation warnings.
    
    * Updated hyperlinks.
    
    * re-evaluate exportin imagery notebook
    
    * Removed Scripts folder.
    
    Co-authored-by: cbur24 <c.a.burton123@gmail.com>

--- Tools/deafrica_tools/app/changefilmstrips.py
+++ Tools/deafrica_tools/app/changefilmstrips.py
@@ -1,4 +1,4 @@
-# notebookapp_changefilmstrips.py
+# changefilmstrips.py
 '''
 This file contains functions for loading and interacting with data in the
 change filmstrips notebook, inside the Real_world_examples folder.
@@ -6,7 +6,7 @@ change filmstrips notebook, inside the Real_world_examples folder.
 Available functions:
     run_filmstrip_app
 
-Last modified: April 2021
+Last modified: October 2021
 '''
 
 # Load modules
