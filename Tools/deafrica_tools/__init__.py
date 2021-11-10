e7895c5a211270a7e5cc19c8a199560855ab0076
andrewdhicks <andrew.d.hicks+github@gmail.com>
2021-11-10 05:50:37 +0000

    Add translation code

--- Tools/deafrica_tools/__init__.py
+++ Tools/deafrica_tools/__init__.py
@@ -1,1 +1,21 @@
 __version__ = "0.1.0"
+
+__locales__ = __path__[0] + '/locales'
+
+
+def set_lang(lang=None):
+    if lang is not None:
+        lang = [lang]
+    
+    import gettext
+    try:
+        translation = gettext.translation(
+            'deafrica_tools', 
+            localedir=__locales__, 
+            languages=lang,
+            fallback=True
+        )
+        translation.install()
+        
+    except FileNotFoundError:
+        print(f'Could not load lang={lang}')
