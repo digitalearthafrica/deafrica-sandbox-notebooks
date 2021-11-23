6af1f525600b28c6aea165e21a8cf5c88b2cf87e
andrewdhicks <andrew.d.hicks+github@gmail.com>
2021-11-23 03:49:21 +0000

    Defaults to using JupterLab language

--- Tools/deafrica_tools/__init__.py
+++ Tools/deafrica_tools/__init__.py
@@ -4,7 +4,14 @@ __locales__ = __path__[0] + '/locales'
 
 
 def set_lang(lang=None):
-    if lang is not None:
+    if lang is None:
+        import os
+        os_lang = os.getenv('LANG')
+        
+        # Just take the first 2 letters: 'fr' not 'fr_FR.UTF-8'
+        if os_lang is not None and len(os_lang) >=2:
+            lang = [os_lang[:2]]
+    else:
         lang = [lang]
     
     import gettext
