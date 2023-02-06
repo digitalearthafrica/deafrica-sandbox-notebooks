c1e80f0702e1c1b02a82b6977dc70fd07a0c6ca5
mpho-sadiki <mpho.sadiki@digitalearthafrica.org>
2023-02-06 06:47:22 +0000

    Upload a GeoJSON or ESRI Shapefile

--- Tools/deafrica_tools/app/areaofinterest.py
+++ Tools/deafrica_tools/app/areaofinterest.py
@@ -1,7 +1,7 @@
 # areaofinterest.py
-'''
+"""
 Upload area of interest as a a GeoJSON or ESRI Shapefile and draw it on a map 
-'''
+"""
 
 import geopandas as gpd
 import ipywidgets as widgets
@@ -21,21 +21,24 @@ def make_box_layout():
         height="100%",
     )
 
+
 class upload_aoi(HBox):
     def __init__(self):
         super().__init__()
         ##################
         # HEADER FOR APP #
         ##################
-      
-        # Add widget to enable uploading a geojson or ESRI shapefile. 
+
+        # Add widget to enable uploading a geojson or ESRI shapefile.
         self.gdf_uploaded = None
         fileupload_aoi = widgets.FileUpload(accept="", multiple=True)
-        # Register the update function to be called for the file upload. 
+        # Register the update function to be called for the file upload.
         fileupload_aoi.observe(self.update_fileupload_aoi, "value")
-        fileupload_html = deawidgets.create_html(value=f"""<h2>Upload a GeoJSON or ESRI Shapefile</h2></br><i></br>Upload a GeoJSON or ESRI Shapefile (<5 mb) containing a single area of interest. When uploading an ESRI shapefile, ensure that all shapefile components (e.g. .shp, .shx, .dbf, .prj) are uploaded.</i>""")
+        fileupload_html = deawidgets.create_html(
+            value=f"""<h2>Upload a GeoJSON or ESRI Shapefile</h2></br><i></br>Upload a GeoJSON or ESRI Shapefile (<5 mb) containing a single area of interest. When uploading an ESRI shapefile, ensure that all shapefile components (e.g. .shp, .shx, .dbf, .prj) are uploaded.</i>"""
+        )
         fileupload = VBox([fileupload_aoi])
-        
+
         ###########################
         # WIDGETS FOR APP OUTPUTS #
         ###########################
@@ -53,7 +56,7 @@ class upload_aoi(HBox):
             zoom_level=3,
         )
         self.m.layout = make_box_layout()
-        
+
         ###############################
         # SPECIFICATION OF APP LAYOUT #
         ###############################
@@ -75,37 +78,41 @@ class upload_aoi(HBox):
         grid[:6, 3:] = self.m
         # Display using HBox children attribute
         self.children = [grid]
-        
+
     def update_fileupload_aoi(self, change):
         # Save to file
         for uploaded_filename in change.new.keys():
             with open(uploaded_filename, "wb") as output_file:
-                content = change.new[uploaded_filename]['content']
-                output_file.write(content)        
-        
+                content = change.new[uploaded_filename]["content"]
+                output_file.write(content)
+
         with self.status_info:
 
-            try:            
-                
+            try:
+
                 valid_files = [
-                    file for file in change.new.keys()
-                    if file.lower().endswith(('.shp', '.geojson'))
+                    file
+                    for file in change.new.keys()
+                    if file.lower().endswith((".shp", ".geojson"))
                 ]
                 valid_file = valid_files[0]
-                aoi_gdf = (gpd.read_file(valid_file).to_crs("EPSG:4326").explode().reset_index(drop=True))
-                
+                aoi_gdf = (
+                    gpd.read_file(valid_file)
+                    .to_crs("EPSG:4326")
+                    .explode()
+                    .reset_index(drop=True)
+                )
+
                 # Create a geodata
-                geodata = GeoData(geo_dataframe=aoi_gdf,
-                                  style={
-                                      'color': 'black',
-                                      'weight': 3
-                                  })
+                geodata = GeoData(
+                    geo_dataframe=aoi_gdf, style={"color": "black", "weight": 3}
+                )
                 # Add to map
                 xmin, ymin, xmax, ymax = aoi_gdf.total_bounds
-                self.m.fit_bounds([[ymin,xmin], [ymax, xmax]])
+                self.m.fit_bounds([[ymin, xmin], [ymax, xmax]])
                 self.m.add_layer(geodata)
-                
-                #Latitude and longitude range class variables
+
+                # Latitude and longitude range class variables
                 upload_aoi.lon_range = (xmin, xmax)
                 upload_aoi.lat_range = (ymin, ymax)
 
@@ -121,17 +128,19 @@ class upload_aoi(HBox):
 
                     # If valid area of interest data returned.
                     if aoi_gdf is not None:
-                        print("Area of interest successfully loaded")                 
-                                      
+                        print("Area of interest successfully loaded")
+
             except IndexError:
                 print(
                     "Cannot read uploaded files. Please ensure that data is "
                     "in either GeoJSON or ESRI Shapefile format.",
-                    end='\r')
+                    end="\r",
+                )
                 self.gdf_uploaded = None
 
             except fiona.errors.DriverError:
                 print(
                     "Shapefile is invalid. Please ensure that all shapefile "
                     "components (e.g. .shp, .shx, .dbf, .prj) are uploaded.",
-                    end='\r')
+                    end="\r",
+                )
