3141e7493b582612e07188b41505e8f5b250f94d
vikineema <victoria@kartoza.com>
2023-09-13 17:02:58 +0300

    Move imports from within functions to the top of the file in dask.py

--- Tools/deafrica_tools/dask.py
+++ Tools/deafrica_tools/dask.py
@@ -7,8 +7,10 @@ from importlib.util import find_spec
 
 import dask
 from aiohttp import ClientConnectionError
+from dask_gateway import Gateway
 from datacube.utils.dask import start_local_dask
 from datacube.utils.rio import configure_s3_access
+from IPython.display import display
 
 _HAVE_PROXY = bool(find_spec('jupyter_server_proxy'))
 _IS_AWS = ('AWS_ACCESS_KEY_ID' in os.environ or
@@ -51,16 +53,14 @@ def create_local_dask_cluster(spare_mem='3Gb', display_client=True, return_clien
 
     # Show the dask cluster settings
     if display_client:
-        from IPython.display import display
         display(client)
-    
+
     # return the client as an object
     if return_client:
         return client
-   
+
 
 try:
-    from dask_gateway import Gateway
 
     def create_dask_gateway_cluster(profile='r5_L', workers=2):
         """
@@ -80,15 +80,15 @@ try:
         """
         try:
             gateway = Gateway()
-            
+
             # Close any existing clusters
             cluster_names = gateway.list_clusters()
             if len(cluster_names) > 0:
                 print("Cluster(s) still running:", cluster_names)
                 for n in cluster_names:
                     cluster = gateway.connect(n.name)
-                    cluster.shutdown()            
-            
+                    cluster.shutdown()
+
             options = gateway.cluster_options()
             options['profile'] = profile
 
