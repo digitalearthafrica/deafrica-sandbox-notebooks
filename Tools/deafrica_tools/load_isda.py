6d6660b1f934e1b92f64ec7ccbe655780f21f5f3
vikineema <victoria@kartoza.com>
2023-09-13 19:15:11 +0300

    Format load_isda.py using black

--- Tools/deafrica_tools/load_isda.py
+++ Tools/deafrica_tools/load_isda.py
@@ -11,17 +11,18 @@ import xarray as xr
 from pystac import Catalog, stac_io
 
 
-#this function allows us to directly query the data on s3, adapted from iSDA tutorial https://github.com/iSDA-Africa/isdasoil-tutorial/blob/main/iSDAsoil-tutorial.ipynb
+# this function allows us to directly query the data on s3, adapted from iSDA tutorial https://github.com/iSDA-Africa/isdasoil-tutorial/blob/main/iSDAsoil-tutorial.ipynb
 def my_read_method(uri):
     parsed = urlparse(uri)
-    if parsed.scheme == 's3':
+    if parsed.scheme == "s3":
         bucket = parsed.netloc
         key = parsed.path[1:]
-        s3 = boto3.resource('s3')
+        s3 = boto3.resource("s3")
         obj = s3.Object(bucket, key)
-        return obj.get()['Body'].read().decode('utf-8')
+        return obj.get()["Body"].read().decode("utf-8")
     else:
         return stac_io.default_read_text_method(uri)
+
 
 stac_io.read_text_method = my_read_method
 
@@ -35,13 +36,14 @@ for root, catalogs, items in catalog.walk():
         # save all items to a dictionary as we go along
         assets[item.id] = item
         for asset in item.assets.values():
-            if asset.roles == ['data']:
+            if asset.roles == ["data"]:
                 str(f"Title: {asset.title}")
                 str(f"Description: {asset.description}")
                 str(f"URL: {asset.href}")
                 str("------------")
 
 # define load_isda() function
+
 
 def load_isda(var, lat, lon):
     """
@@ -53,34 +55,36 @@ def load_isda(var, lat, lon):
     lat: tuple or list
         Latitude range for query.
     lon: tuple or list
-        Longitude range for query. 
+        Longitude range for query.
     """
 
-    bands = assets[var].assets["image"].extra_fields.get('eo:bands')
-    bands = [val['description'] for val in bands]
-    
+    bands = assets[var].assets["image"].extra_fields.get("eo:bands")
+    bands = [val["description"] for val in bands]
+
     if len(np.unique(bands)) > 1:
-    
         ds = xr.open_dataset(assets[var].assets["image"].href, engine="rasterio").rio.clip_box(
+            minx=lon[0],
+            miny=lat[0],
+            maxx=lon[1],
+            maxy=lat[1],
+            crs="EPSG:4326",
+        )
+
+        ds_layered = ds.drop_dims("band")
+        for x in np.unique(ds.band):
+            ds_layered[bands[x - 1]] = ds.sel(band=x).to_array(dim="band").squeeze()
+
+    else:
+        ds_layered = (
+            xr.open_dataset(assets[var].assets["image"].href, engine="rasterio")
+            .rio.clip_box(
                 minx=lon[0],
                 miny=lat[0],
                 maxx=lon[1],
                 maxy=lat[1],
                 crs="EPSG:4326",
             )
-
-        ds_layered = ds.drop_dims('band')
-        for x in np.unique(ds.band):
-            ds_layered[bands[x-1]] = ds.sel(band=x).to_array(dim='band').squeeze()
-            
-    else:
-        
-        ds_layered = xr.open_dataset(assets[var].assets["image"].href, engine="rasterio").rio.clip_box(
-                minx=lon[0],
-                miny=lat[0],
-                maxx=lon[1],
-                maxy=lat[1],
-                crs="EPSG:4326",
-            ).squeeze()
+            .squeeze()
+        )
 
     return ds_layered
