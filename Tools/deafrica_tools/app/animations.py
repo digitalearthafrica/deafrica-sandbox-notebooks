4524eccbed6a53e61945bc9f96674debc62ec345
Victoria Neema <39700992+vikineema@users.noreply.github.com>
2024-10-17 10:10:51 +0300

    Sandbox image upgrade (#552)
    * Remove manual setting of variable USE_PYGEOS
    
    * Bump version
    
    * Remove flake8 constraint
    
    * Fix ValueError in subpixel_contours
    
    * Update build system dependencies
    
    * Set crop default to False
    
    * Bump version
    
    * Update setuptools requirement
    
    * Fix apps not available in 2.5.1
    
    * Fix RecursionError in Intertidal_elevation notebook
    
    * Update Change_filmstrips notebook
    
    * Fix recursion error
    
    * Fix crop parameter
    
    * Update window size for lee filter
    
    * Regenerate model file
    
    * Update phenology plotting
    
    * Bump version
    
    * Update readme for tools
    
    * Update conda environment file to match sandbox upgrade
    
    * Update python version for precommit
    
    * Fix parallel_apply
    
    * Remove environment variable USE_PYGEOS

--- Tools/deafrica_tools/app/animations.py
+++ Tools/deafrica_tools/app/animations.py
@@ -3,13 +3,6 @@
 Satellite imagery animation widget, which can be used to interactively
 produce animations for multiple DE Africa products.
 """
-
-# Force GeoPandas to use Shapely instead of PyGEOS
-# In a future release, GeoPandas will switch to using Shapely by default.
-import os
-
-os.environ["USE_PYGEOS"] = "0"
-
 import datetime
 import itertools
 import json
